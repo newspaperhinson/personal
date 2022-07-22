420ec4453109c8b8d4fa35e31ea8b5f95dacca9d
LAPTOP-UDCCADMA\ROG <hinsonpo@gffclub.com>
2022-07-22 17:58:45 +0800

    .

--- study/react/index.js
+++ study/react/index.js
@@ -31,3 +31,109 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //   }
 // }
 // root.render(<Clock/>);
+
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggleOn: true};
+//         //This binding is necessary to make `this` work in the callback
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+//     handleClick() {
+//         this.setState(prevState => ({
+//             isToggleOn: !prevState.isToggleOn
+//         }));
+//     }
+//     render() {
+//         return (
+//             <button onClick={this.handleClick}>
+//                 {this.state.isToggleOn? "ON": "OFF"}
+//             </button>
+//         );
+//     }
+// }
+// root.render(<Toggle/>);
+
+// function LoginButton(props) {
+//     return (
+//       <button onClick={props.onClick}>
+//         Login
+//       </button>
+//     );
+//   }
+  
+// function LogoutButton(props) {
+//     return (
+//       <button onClick={props.onClick}>
+//         Logout
+//       </button>
+//     );
+// }
+// class LoginControl extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.handleLoginClick = this.handleLoginClick.bind(this);
+//       this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//       this.state = {isLoggedIn: false};
+//     }
+  
+//     handleLoginClick() {
+//       this.setState({isLoggedIn: true});
+//     }
+  
+//     handleLogoutClick() {
+//       this.setState({isLoggedIn: false});
+//     }
+  
+//     render() {
+//       const isLoggedIn = this.state.isLoggedIn;
+//       let button;
+//       if (isLoggedIn) {
+//         button = <LogoutButton onClick={this.handleLogoutClick} />;
+//       } else {
+//         button = <LoginButton onClick={this.handleLoginClick} />;
+//       }
+  
+//       return (
+//         <div>
+//           <Greeting isLoggedIn={isLoggedIn} />
+//           {button}
+//         </div>
+//       );
+//     }
+// }
+  
+// root.render(<LoginControl />);
+
+function WarningBanner(props) {
+    if(!props.warn)
+        return null;
+    return (
+        <div className="warning">
+            Warning!
+        </div>
+    )
+}
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showWarning: true};
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+    handleToggleClick() {
+        this.setState(state => ({showWarning: !state.showWarning}));
+    }
+    render() {
+        return (
+            <div>
+                <WarningBanner warn={this.state.showWarning}/>
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning? "Hide": "Show"}
+                </button>
+            </div>
+        );
+    }
+}
+
+root.render(<Page/>);
