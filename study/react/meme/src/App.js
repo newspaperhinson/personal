409892b6af53628b626a52cd70011f25564dd1a7
Hinson <newspaperhinson@gmail.com>
2022-08-04 01:44:27 +0800

    finished header and started meme section

--- study/react/meme/src/App.js
+++ study/react/meme/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 function App() {
   return (
-    
+    <div>
+      <Header />
+      <Meme />
+    </div>
   );
 }
 
